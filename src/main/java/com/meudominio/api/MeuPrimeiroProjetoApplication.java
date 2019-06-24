1699f168ecbc37bbfc2211710355ce78b96b11bf
Marco <marco_sidro@hotmail.com>
2019-06-24 12:05:53 -0300

    JPA/MySQL/H2/App/Profs/BCrypt

--- src/main/java/com/meudominio/api/MeuPrimeiroProjetoApplication.java
+++ src/main/java/com/meudominio/api/MeuPrimeiroProjetoApplication.java
@@ -2,13 +2,36 @@ package com.meudominio.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+
+import com.meudominio.api.utils.SenhaUtils;
+
+import org.springframework.boot.CommandLineRunner;
 
 @SpringBootApplication
 public class MeuPrimeiroProjetoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MeuPrimeiroProjetoApplication.class, args);
-		System.out.println("Meu primeiro Projeto...");
+		
 	}
+	
+	@Bean
+	public CommandLineRunner commandLineRunner() {
+		return args -> {
+			String senhaEncoded = SenhaUtils.gerarBCrypt("123456");
+			System.out.println("Senha Ecoded: " +senhaEncoded);
+			
+			senhaEncoded = SenhaUtils.gerarBCrypt("123456");
+			System.out.println("Senha encoded novamente: " + senhaEncoded);
+			
+			System.out.println("senha válida: " + SenhaUtils.senhaValida("123456", senhaEncoded));
+			
+		};
+		
+	}
+	
+	
 
 }
